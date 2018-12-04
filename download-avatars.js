394964413d9e34cf1327819a4d4fb6428cd89bd4
Negar Amiri <negamiri@gmail.com>
2018-12-04 02:37:10 +0000

    function created for downloading a sample image

--- download-avatars.js
+++ download-avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var tokens = require('./secrets.js')
+var fs = require('fs');
+
 
 console.log('Welcome to the Github Avatar Downloader!')
 
@@ -21,10 +23,18 @@ function getRepoContributors(repoOwner, repoName, cb){
 
 }
 
+// function downloadImageByURL(url, filepath){
+  request.get("https://avatars2.githubusercontent.com/u/2741?v=3&s=466")
+          .on('error', function(err){
+            throw err;
+          })
+          .pipe(fs.createWriteStream("./example.jpg"))
+// }
+
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Error: " + err);
   result.forEach(function(item){
-    console.log("Avatar URL: " + item.avatar_url);
+    downloadImageByURL(item.avatar_url, filepath);
   })
 
 })
