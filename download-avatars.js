70bc6851eb97fd1e0101bc32c3125673b1479ce9
Negar Amiri <negamiri@gmail.com>
2018-12-04 01:52:11 +0000

    Token added with secret file added to .gitignore

--- download-avatars.js
+++ download-avatars.js
@@ -1,8 +1,21 @@
 var request = require('request');
+var tokens = require('./secrets.js')
 
 console.log('Welcome to the Github Avatar Downloader!')
 
 function getRepoContributors(repoOwner, repoName, cb){
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + "repoName" + "/contributors",
+    headers: {
+      'User-agent': 'request',
+      'Accesskey': 'token ' + tokens.GITHUB_TOKEN
+    }
+  };
+
+  request(url, function(err, res, body){
+    cb(err, body);
+  });
 
 }
 
