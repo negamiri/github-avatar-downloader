4c1684e62e061c9234f6cd86dc2bbb586ef75dd7
Negar Amiri <negamiri@gmail.com>
2018-12-04 02:20:17 +0000

    Parsed JSON string and called Avatar URLs

--- download-avatars.js
+++ download-avatars.js
@@ -6,20 +6,25 @@ console.log('Welcome to the Github Avatar Downloader!')
 function getRepoContributors(repoOwner, repoName, cb){
 
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + "repoName" + "/contributors",
+    url: "https://api.github.com/repos/" + repoOwner + "/" +repoName + "/contributors",
     headers: {
       'User-agent': 'request',
-      'Accesskey': 'token ' + tokens.GITHUB_TOKEN
+      'Authorization':' token ' + tokens.GITHUB_TOKEN
+
     }
   };
 
-  request(url, function(err, res, body){
-    cb(err, body);
+  request(options, function(err, res, body){
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
-  console.log("Errors: " + err);
-  console.log("Result: " + result);
+  console.log("Error: " + err);
+  result.forEach(function(item){
+    console.log("Avatar URL: " + item.avatar_url);
+  })
+
 })
